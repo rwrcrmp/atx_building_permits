ff19a3347b7dff5519b365200e69004213f336f9
Robert Crump <119753396+rwrcrmp@users.noreply.github.com>
2023-09-04 16:38:42 -0500

    clean up

--- R/setup.R
+++ R/setup.R
@@ -1,30 +1,38 @@
-# this script installs all required packages
-# and builds out a repo directory structure
-# for an initial commit. delete temp.txt files
-# after first git commit
+### this script installs packages and builds directory structure for an initial git commit ###
 
-# install packages
+## install packages
+
+# list packages you need for your project, update as needed
 packages <- c('tidyverse', 'sf', 'renv')
 
+# iterate over character vector to install packages
 lapply(packages, install.packages)
 
-rm(packages)
-
+# when necessary, install packages from source
 install.packages("E:/R/RSocrata_1.7.12-4.tar.gz", repos = NULL, type = "source")
 
+# capture packages in `renv` lockfile
 renv::snapshot()
 
-# build repo directory
+## build repo directory
 folders <- c("data", "data/extracted_files", "data/created_files", "data/archive", 
              "R", "R/archive", 
              "assets", "assets/images", 
              "products", "products/documents", "products/html")
 
+# iterate over character vector to create folders and subfolders
 lapply(folders, dir.create)
 
+# create temporary files for initial commit
 for (i in folders){
   x <- paste0(i, "/temp.txt")
   file.create(x)
 }
 
-rm(folders)
+# after pushing initial commit remove temp files and R objects
+for (i in folders){
+  x <- paste0(i, "/temp.txt")
+  file.remove(x)
+}
+
+rm(list = ls())
